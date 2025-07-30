26e32a11f053df5e8a59618dd70d202f58901385
denake-on <denake37@gmail.com>
2025-07-30 14:01:52 +0800

    Update Headline.tsx

--- app/(main)/Headline.tsx
+++ app/(main)/Headline.tsx
@@ -3,48 +3,21 @@
 import { motion } from 'framer-motion'
 import Balancer from 'react-wrap-balancer'
 
-import { SparkleIcon, UserSecurityIcon } from '~/assets'
-import { PeekabooLink } from '~/components/links/PeekabooLink'
+import { SparkleIcon } from '~/assets'
+// import { UserSecurityIcon } from '~/assets'
+// import { PeekabooLink } from '~/components/links/PeekabooLink'
 import { SocialLink } from '~/components/links/SocialLink'
 
-function Developer() {
-  return (
-    <span className="group">
-      <span className="font-mono">&lt;</span>开发者
-      <span className="font-mono">/&gt;</span>
-      <span className="invisible inline-flex text-zinc-300 before:content-['|'] group-hover:visible group-hover:animate-typing dark:text-zinc-500" />
-    </span>
-  )
-}
-
-function Designer() {
-  return (
-    <span className="group relative bg-black/5 p-1 dark:bg-white/5">
-      <span className="pointer-events-none absolute inset-0 border border-lime-700/90 opacity-70 group-hover:border-dashed group-hover:opacity-100 dark:border-lime-400/90">
-        <span className="absolute -left-[3.5px] -top-[3.5px] size-1.5 border border-lime-700 bg-zinc-50 dark:border-lime-400" />
-        <span className="absolute -bottom-[3.5px] -right-[3.5px] size-1.5 border border-lime-700 bg-zinc-50 dark:border-lime-400" />
-        <span className="absolute -bottom-[3.5px] -left-[3.5px] size-1.5 border border-lime-700 bg-zinc-50 dark:border-lime-400" />
-        <span className="absolute -right-[3.5px] -top-[3.5px] size-1.5 border border-lime-700 bg-zinc-50 dark:border-lime-400" />
-      </span>
-      设计师
-    </span>
-  )
-}
+// 以下角色部分註釋掉：
+// function Developer() { ... }
+// function Designer() { ... }
+// function Founder() { ... }
 
 function OCD() {
   return (
     <span className="group inline-flex items-center">
       <SparkleIcon className="mr-1 inline-flex transform-gpu transition-transform duration-500 group-hover:rotate-180" />
-      <span>如果记忆是一罐罐头</span>
-    </span>
-  )
-}
-
-function Founder() {
-  return (
-    <span className="group inline-flex items-center">
-      <UserSecurityIcon className="mr-1 inline-flex group-hover:fill-zinc-600/20 dark:group-hover:fill-zinc-200/20" />
-      <span>创始人</span>
+      <span>twenty to infinity</span>
     </span>
   )
 }
@@ -63,10 +36,12 @@ export function Headline() {
           duration: 0.3,
         }}
       >
-        <Developer />，<Designer />，
-        <span className="block h-2" />
-        <OCD />，<Founder />
+        {/* <Developer />，<Designer />， */}
+        {/* <span className="block h-2" /> */}
+        <OCD />
+        {/* ，<Founder /> */}
       </motion.h1>
+
       <motion.p
         className="mt-6 text-base text-zinc-600 dark:text-zinc-400"
         initial={{ opacity: 0, y: 20 }}
@@ -79,13 +54,9 @@ export function Headline() {
           delay: 0.1,
         }}
       >
-        <Balancer>
-          我是 Cali，
-          <PeekabooLink href="https://zolplay.com">佐玩</PeekabooLink>
-          创始人，目前带领着佐玩致力于创造一个充满创造力的工作环境，同时鼓励团队创造影响世界的产品。
-          我热爱开发，设计，创新，享受生活，以及在未知领域中探索。
-        </Balancer>
+        <Balancer>往四處走走</Balancer>
       </motion.p>
+
       <motion.div
         className="mt-6 flex gap-6"
         initial={{ opacity: 0, y: 10 }}
